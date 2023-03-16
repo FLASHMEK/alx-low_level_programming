96e3f32470a5001cf9bf57001dc75b4e6786d989
FLASHMEK <fmekgwe@gmail.com>
2023-03-16 15:19:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - is the main function
  *
  * Return: is always 0
- * \
+ */
  int main(void)
- { puts("\"Programming is like building a multilingual puzzle");
+
+ { puts("\"Programming is like building a multilingual puzzle);
  return (0); }
