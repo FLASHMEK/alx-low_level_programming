2598424870af0a0eb27c87cf345347d64f827076
FLASHMEK <fmekgwe@gmail.com>
2023-03-17 14:57:46 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase,
+ * follwed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
