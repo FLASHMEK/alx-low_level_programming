2593752ec46d93f4a149fdce091138527038695c
FLASHMEK <fmekgwe@gmail.com>
2023-03-28 08:49:59 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  Owned By festus Sulumeti
-*  Return: 0 is success
-*/
-
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * also updates the value it points to to 98
+ * @n: input
+ * Return: n
+ */
 void reset_to_98(int *n)
 {
-        *n = 98;
+	*n = 98;
 }
