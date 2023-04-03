3157e68ca1b392be92b512c12e714b1c007d011a
FLASHMEK <fmekgwe@gmail.com>
2023-04-03 13:24:19 -0700

    memcypy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * _memcpy - a function that copies memorey area
- * @dest: stored memory
- * @srs: copied memory
- * @n: number of bytes
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
  *
- * Return: copied memory with n bytes changed
+ *Return: copied memory with n bytes changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
 	int r = 0;
 	int i = n;
@@ -18,4 +17,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		dest[r] = src[r];
 		n--;
 	}
+	return (dest);
 }
