db19a66a453340439814da6230d5299423ec3b17
FLASHMEK <fmekgwe@gmail.com>
2023-04-03 13:43:15 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strchr - Point of entry
+ * _strchr - Entry point
  * @s: input
  * @c: input
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 
