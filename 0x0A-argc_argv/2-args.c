3e079eadf9f14f25b32c1d636a6b471378d65393
FLASHMEK <fmekgwe@gmail.com>
2023-04-09 10:59:02 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints all arguments it receivES
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
