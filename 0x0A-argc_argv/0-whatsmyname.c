59968df7afb824e11ee5ca4d665d198cf4ef3704
FLASHMEK <fmekgwe@gmail.com>
2023-04-09 10:20:12 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints name
+ * @argc: argument number
+ * @argv: argument vector
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
