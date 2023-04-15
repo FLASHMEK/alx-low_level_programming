6cddece600b179bb8adb8732e7c19abde4894237
FLASHMEK <fmekgwe@gmail.com>
2023-04-15 08:38:17 -0700

    1-memcpy.c clear . .. clear exit < > end cd .. root

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -3,8 +3,24 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+ *_memcpy - compy memory area
+ * @dest: stored memory
+ * @src: memory to copy
+ * @n: bytes
+ *
+ * Return: copied memory
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return memcpy(dest, src, n);
+	int z = 0;
+	int p = n;
+
+	for (;z < p; z++)
+	{
+		dest[z] = src[z];
+
+		n--;
+	}
+	return (dest);
 }
