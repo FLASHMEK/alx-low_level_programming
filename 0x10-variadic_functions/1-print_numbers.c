f231f1657930e02d35588b9b7e7e3c1a07909c42
FLASHMEK <fmekgwe@gmail.com>
2023-04-20 14:59:33 -0700

    variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_numbers - print numbers given as parameters
@@ -10,19 +12,20 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+
 	va_list list;
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (!separator)
+		if(!separator)
 			printf("%d", va_arg(list, int));
-		else if (separator && i == 0)
+		else if (separator && i== 0)
 			printf("%d", va_arg(list, int));
 		else
 			printf("%s%d", separator, va_arg(list, int));
 	}
-	va_end(list;
-			printf("\n");
-			}
+	va_end(list);
+	printf("\n");
+}
